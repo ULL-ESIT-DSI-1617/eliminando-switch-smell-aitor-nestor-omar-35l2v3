9620de707f808305e22137c731a2895ba1ac153b
Nestor <alu0100812503@ull.edu.es>
2017-04-05 19:06:33 +0100

    Encapsulando codigo

--- client/js/temperatura.js
+++ client/js/temperatura.js
@@ -21,3 +21,72 @@ class Temperatura extends Medida{
         super.unit = x;
     }
 }
+
+class Kelvin extends Temperatura{
+    constructor(x){
+        super(x)
+    }
+
+    toCelsius(){                          //Paso de Kelvin a Celsius
+        let cel = super.get_val();
+        let aux = cel - 273.15;
+        return aux;
+
+    }
+
+    toKelvin(){
+        return super.get_val();
+    }
+
+    toFahrenheit(){                       //Paso de Kelvin a Fahrenheit
+        let cel = super.get_val();
+        let aux = cel * (9/5) - 459.67;
+        return aux;
+    }
+}
+class Fahrenheit extends Temperatura{
+    constructor(x){
+        super(x)
+    }
+
+    toCelsius(){                          //Paso de Fahrenheit a Celsius
+        let cel = super.get_val();
+        let aux = cel - 32 * (5/9);
+        return aux;
+    }
+
+    toKelvin(){                           //Paso de Fahrenheit a Kelvin
+        let cel = super.get_val();
+        let aux = parseInt(cel) - 32 * (5/9) + 273;
+        return aux;
+    }
+
+    toFahrenheit(){
+        return super.get_val();
+    }
+}
+
+class Celsius extends Temperatura{
+    constructor(x){
+        super(x);
+
+    }
+
+    toFahrenheit(){
+        let cel = super.get_val()
+        let aux = cel * (9/5) + 32;
+        return aux;
+    }
+
+    toKelvin(){
+        let cel = super.get_val();
+        let aux = parseInt(cel) + 273.15;
+        return aux;
+    }
+
+    toCelcius(){
+        return super.get_val();
+    }
+
+
+}
