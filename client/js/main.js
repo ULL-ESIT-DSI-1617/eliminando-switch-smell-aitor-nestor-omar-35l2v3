955f86feaf15f73977e07ac2c0a9b01f6d3e01a7
Nestor <alu0100812503@ull.edu.es>
2017-04-15 21:02:01 +0100

    Finalizado por fin

--- client/js/main.js
+++ client/js/main.js
@@ -1,9 +1,5 @@
 function main() {
       var valor     = document.getElementById('original').value,
           elemento  = document.getElementById('converted');
-                  p = new Medida(valor)
-                  elemento.innerHTML =  p.convertir(valor);
-      return false;
-
-  
+          elemento.innerHTML =  calcular(valor)
 }
